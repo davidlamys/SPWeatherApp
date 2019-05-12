994f20a2b4389a7c2cb6ece13fea9be2acfb9b8a
david lam <david.lys@gmail.com>
2019-05-12 18:06:52 +0800

    Utility function to convert email address to md5 hash

---
+++ PDContactListTests/UtilitiesTests.swift
@@ -0,0 +1,24 @@
+//
+//  UtilitiesTests.swift
+//  PDContactListTests
+//
+//  Created by David_Lam on 12/5/19.
+//  Copyright © 2019 David_Lam. All rights reserved.
+//
+
+import XCTest
+@testable import PDContactList
+
+class UtilitiesTests: XCTestCase {
+
+    func testPerformanceExample() {
+        self.measure {
+            let _ = getMD5Hex(string: "davidlys@gmail.com")
+        }
+    }
+
+    func testMD5FunctionShouldReturnCorrectValue() {
+        let subject = getMD5Hex
+        assert(subject("davidlys@gmail.com") == "b5f6c2a999a22d0204532a6ede7ba92d")
+    }
+}
