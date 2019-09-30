1fb5162bcdbc45d8ccf9e1f652b516004e68da48
david lam <david.lys@gmail.com>
2019-09-30 21:57:15 +0800

    Delete LocalStorageProviderImplementation and exclude LocalStorageProviderTest

--- SPWeatherApp/Services/LocalStorageProvider.swift
+++ SPWeatherApp/Services/LocalStorageProvider.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2019 David_Lam. All rights reserved.
 //
 
-import CoreData
 import UIKit
 
 protocol LocalStorageProviderType {
@@ -16,62 +15,18 @@ protocol LocalStorageProviderType {
 }
 
 class LocalStorageProvider {
-
-    let persistentContainer: NSPersistentContainer!
-
-    // MARK: Init with dependency
-    init(container: NSPersistentContainer) {
-        self.persistentContainer = container
-        self.persistentContainer.viewContext.automaticallyMergesChangesFromParent = true
-    }
-
-    convenience init() {
-        //Use the default container for production environment
-        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
-            fatalError("Can not get shared app delegate")
-        }
-        self.init(container: appDelegate.persistentContainer)
+    init() {
     }
 }
 
 extension LocalStorageProvider: LocalStorageProviderType {
 
     func getListItemsFromLocal(completion: @escaping ((Items) -> Void)) {
-        let request = PostObject.fetchRequest
-        request.sortDescriptors = [NSSortDescriptor(key: "id", ascending: true)]
-        let backgroundContext = persistentContainer.newBackgroundContext()
-        do {
-            guard let result = try backgroundContext.fetch(request) as? [PostObject] else {
-                fatalError("core data is not configured properly")
-            }
-
-            completion(Translator.translateFromCoreDataObject(objects: result))
-        } catch let error {
-            logError(error)
-            completion([])
-        }
     }
 
     func insertListItems(data newList: Items) {
-        let backgroundContext = persistentContainer.viewContext
-        newList.forEach({ PostObject.insert(into: backgroundContext, post: $0) })
-        do {
-            try backgroundContext.save()
-        } catch let error {
-            logError(error)
-        }
     }
 
     func deleteListItems() {
-        let fetch = PostObject.fetchRequest
-
-        let request = NSBatchDeleteRequest(fetchRequest: fetch)
-        let backgroundContext = persistentContainer.newBackgroundContext()
-        do {
-            _ = try backgroundContext.execute(request)
-            try backgroundContext.save()
-        } catch let error {
-            logError(error)
-        }
     }
 }
